614c5a921b7bee1883d41aa1ab3e196e3ecd8167
Mariusz <grebelsm@tcd.ie>
2018-05-19 15:14:52 +0100

    fixed recommendation call

--- script.py
+++ script.py
@@ -19,15 +19,17 @@ key = 0.0
 loud = 0.0
 val = 0.0
 
-LIMIT = 20
+LIMIT = 5
 
 spotify_token = util.prompt_for_user_token(username, scope, client_id='f25193ec5e474898966a8a2e1cda9df2', client_secret='67d776c422b24b33b064159e39764873', redirect_uri='https://example.com/callback/')
 
 if spotify_token:
     sp = spotipy.Spotify(auth=spotify_token)
     results = sp.current_user_top_tracks(limit=LIMIT)
+    results2 = sp.current_user_top_artists(limit=LIMIT)
     tids = []
     print(json.dumps(results, indent=4))
+    print(json.dumps(results2, indent=4))
     for i, t in enumerate(results['items']):
        tids.append(t['uri'])
 
@@ -44,7 +46,7 @@ if spotify_token:
         loud += feature['loudness']
         val += feature['valence']
 
-        print(json.dumps(feature, indent=4))
+        #print(json.dumps(feature, indent=4))
     
     nrg = nrg/LIMIT
     live = live/LIMIT
@@ -56,9 +58,18 @@ if spotify_token:
     key = key/LIMIT
     loud = loud/LIMIT
     val = val/LIMIT
-
-    recommended = sp.recommendations(target_energy=nrg, target_liveness=live, target_tempo=tempo, target_speechiness=speech, target_acousticness=acous, target_instrumentalness=inst, target_danceability=dance, target_key=key, target_loudness=loud, target_valence=val)
-    print(json.dumps(recommend, indent=4))
+    
+    genres = sp.recommendation_genre_seeds()
+    print(json.dumps(genres, indent=4))
+    genreList = []
+    artistList = []
+    trackList = []
+    for artist in results2['items']:
+        artistList.append(artist['id'])
+    recommended = sp.recommendations(seed_artists = artistList, target_energy = nrg, target_liveness = live, target_tempo = tempo, target_speechiness = speech, target_acousticness = acous, target_instrumentalness = inst, target_danceability = dance, limit=1)
+    print(json.dumps(recommended, indent=4))
+    for i in recommended['tracks']:
+        print i['uri']
   
 
 else:
